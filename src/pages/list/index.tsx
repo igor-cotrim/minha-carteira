ffda17d40c75345d6a0eb69524cb5b8f587ea6bb
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-02-17 14:21:11 -0300

    create filter

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -5,17 +5,40 @@ import SelectInput from "../../components/select-input"
 import * as S from "./styled"
 
 const List = () => {
-  const options = [
-    { value: 'Igor', label: 'Igor' },
-    { value: 'Thiago', label: 'Thiago' },
-    { value: 'Felipe', label: 'Felipe' },
+  const months = [
+    { value: 7, label: 'Julho' },
+    { value: 8, label: 'Agosto' },
+    { value: 9, label: 'Setembro' },
+  ]
+
+  const years = [
+    { value: 2021, label: 2021 },
+    { value: 2020, label: 2020 },
+    { value: 2019, label: 2019 },
   ]
 
   return (
     <S.Container>
       <ContentHeader title="Saídas" lineColor="#e44c4e">
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <S.Filters>
+        <button
+          type="button"
+          className="tag-filter tag-filter-recurrent"
+        >
+          Recorrents
+        </button>
+        <button
+          type="button"
+          className="tag-filter tag-filter-eventual"
+        >
+          Eventuais
+        </button>
+      </S.Filters>
+
       <S.Content>
         <HistoryFinanceCard
           tagColor="#e44c4e"
--- src/pages/list/styled.ts
+++ src/pages/list/styled.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   
@@ -6,4 +6,42 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   
+`;
+
+export const Filters = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+
+    .tag-filter{
+      font-size: 18px;
+      font-weight: 500;
+      background: none;
+      color: ${theme.colors.white};
+      margin: 0 10px;
+      transition: opacity 0.3s;
+
+      &:hover{
+        opacity: 0.7;
+      }
+    }
+
+    .tag-filter-recurrent::after{
+      content: '';
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+      border-bottom: 10px solid ${theme.colors.warning};
+    }
+
+    .tag-filter-eventual::after{
+      content: '';
+      display: block;
+      width: 55px;
+      margin: 0 auto;
+      border-bottom: 10px solid ${theme.colors.success};
+    }
+  `}
 `;
