de9d2372a780dee3f2e8946fb66b24312e8fb188
Igor Cotrim <igorcotrim.dev@gmail.com>
2022-02-16 21:42:31 -0300

    create lateral menu

--- src/components/aside/index.tsx
+++ src/components/aside/index.tsx
@@ -1,9 +1,38 @@
+import {
+  MdDashboard,
+  MdArrowUpward,
+  MdArrowDownward,
+  MdExitToApp
+} from 'react-icons/md'
+
 import * as S from "./styles"
 
 const Aside = () => {
   return (
     <S.Container>
-      <h1>Aside</h1>
+      <S.Header>
+        <S.Logo />
+        <S.Title>Minha Carteira</S.Title>
+      </S.Header>
+
+      <S.MenuContainer>
+        <S.MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </S.MenuItemLink>
+        <S.MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </S.MenuItemLink>
+      </S.MenuContainer>
     </S.Container>
   )
 }
--- src/components/aside/styles.ts
+++ src/components/aside/styles.ts
@@ -1,9 +1,56 @@
 import styled, { css } from 'styled-components';
 
+import LogoSvg from '../../assets/svg-components/logo';
+
 export const Container = styled.div`
-  ${({theme}) => css`
+  ${({ theme }) => css`
     grid-area: AS;
-    color: ${theme.colors.white};
     background: ${theme.colors.secondary};
+    padding-left: 20px;
+    border-right: 1px solid ${theme.colors.gray};
+  `}
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 70px;
+`;
+
+export const Logo = styled(LogoSvg)`
+  width: 40px;
+  height: 40px;
+`;
+
+export const Title = styled.h3`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    margin-left: 10px;
+  `}
+`;
+
+export const MenuContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+`;
+
+export const MenuItemLink = styled.a`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    color: ${theme.colors.info};
+    text-decoration: none;
+    transition: opacity 0.3s;
+    margin: 7px 0;
+
+    &:hover{
+      opacity: 0.7;
+    }
+
+    > svg {
+      font-size: 18px;
+      margin-right: 5px;
+    }
   `}
 `;
